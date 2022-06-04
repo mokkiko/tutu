daccd4dfb87d40b70580f4b7a234837a1933635a
mokkiko <68779748+mokkiko@users.noreply.github.com>
2022-06-04 17:50:31 +0300

    add namespace for admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,17 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :tickets, only: [:create, :show]
-
-  resources :railway_stations do
-    patch :update_position, on: :member 
-  end
-
-  resources :trains  
-  resources :routes
-
   resource :search, only: [:new, :show, :edit]
+
+  namespace :admin do
+    resources :railway_stations do
+      patch :update_position, on: :member 
+    end
+    resources :trains  
+    resources :routes
+    resources :tickets
+  end 
+
 
   get 'welcome/index'
   root 'welcome#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_04_140918) do
+ActiveRecord::Schema.define(version: 2022_06_04_144100) do
 
   create_table "carriages", force: :cascade do |t|
     t.integer "number"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2022_06_04_140918) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
