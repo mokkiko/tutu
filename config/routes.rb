1424bf813adacdd863b532eccf737f6fb4bce2f8
mokkiko <68779748+mokkiko@users.noreply.github.com>
2022-06-04 17:39:13 +0300

    add buy ticket

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :tickets
+
   devise_for :users
+
+  resources :tickets, only: [:create, :show]
+
   resources :railway_stations do
     patch :update_position, on: :member 
   end
--- db/migrate/20220604140918_create_tickets.rb
+++ db/migrate/20220604140918_create_tickets.rb
@@ -2,6 +2,8 @@ class CreateTickets < ActiveRecord::Migration[6.1]
   def change
     create_table :tickets do |t|
 
+      t.belongs_to :user, index: true
+
       t.timestamps
     end
   end
