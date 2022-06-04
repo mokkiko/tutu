b239c14a16540da1b19f75db73463766e6147fa8
mokkiko <68779748+mokkiko@users.noreply.github.com>
2022-06-04 10:53:32 +0300

    add search controller

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -2,4 +2,6 @@ class RailwayStation < ApplicationRecord
   has_many :trains
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes  
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :trains  
   resources :routes
 
+  resource :search, only: [:new, :show, :edit]
+
   get 'welcome/index'
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
