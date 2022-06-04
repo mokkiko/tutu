403038632cb5df6a6f63695d24a5b031392aa1f0
mokkiko <68779748+mokkiko@users.noreply.github.com>
2022-06-04 16:58:50 +0300

    customize devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,9 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
@@ -70,6 +73,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.delivery_method = :letter_opener
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :railway_stations do
     patch :update_position, on: :member 
   end
